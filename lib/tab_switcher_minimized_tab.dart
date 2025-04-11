f95aa6c8a7de06446beb279ca64b339939468b22
Mahdi Pishguy <pishguy@gmail.com>
2025-04-11 11:25:26 +0330

    Update tab_switcher_minimized_tab.dart

--- lib/tab_switcher_minimized_tab.dart
+++ lib/tab_switcher_minimized_tab.dart
@@ -6,118 +6,114 @@ import 'package:tab_switcher/ui_image_widget.dart';
 /// Consists of title, subtitle, preview image and a close button.
 /// Implements swipe to dismiss on it's own.
 class TabSwitcherMinimizedTab extends StatelessWidget {
-  TabSwitcherMinimizedTab(this._tab, this.onTap, this.onClose, this._isCurrent);
+  const TabSwitcherMinimizedTab({
+    Key? key,
+    required this.tab,
+    required this.onTap,
+    required this.onClose,
+    required this.isCurrent,
+  }) : super(key: key);
 
-  final VoidCallback onClose;
+  final TabSwitcherTab tab;
   final VoidCallback onTap;
-  final TabSwitcherTab _tab;
-  final bool _isCurrent;
+  final VoidCallback onClose;
+  final bool isCurrent;
 
   @override
   Widget build(BuildContext context) {
-    final title = _tab.getTitle();
-    final subtitle = _tab.getSubtitle();
+    final title = tab.getTitle();
+    final subtitle = tab.getSubtitle();
     final theme = Theme.of(context);
     final colorScheme = theme.colorScheme;
 
     return Dismissible(
-      movementDuration: Duration(milliseconds: 1),
-      resizeDuration: Duration(milliseconds: 1),
-      key: ValueKey(_tab.index),
+      key: ValueKey(tab.index),
       onDismissed: (direction) => onClose(),
       child: GestureDetector(
         onTap: onTap,
-        child: Container(
+        child: DecoratedBox(
           decoration: BoxDecoration(
-            color: _isCurrent ? colorScheme.primary : colorScheme.surface,
+            color: isCurrent ? colorScheme.primary : colorScheme.surface,
             borderRadius: BorderRadius.circular(10),
-            boxShadow: [
+            boxShadow: const [
               BoxShadow(color: Colors.black12, spreadRadius: 1.5, blurRadius: 4),
             ],
           ),
           child: Column(
             children: [
               Padding(
-                padding: EdgeInsets.only(left: 8, right: 4, top: 4, bottom: 0),
+                padding: const EdgeInsets.fromLTRB(8, 4, 4, 0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Expanded(
                       child: Text(
                         title,
-                        style:
-                            TextStyle(fontSize: 14, color: _isCurrent ? colorScheme.onPrimary : colorScheme.onSurface),
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: isCurrent ? colorScheme.onPrimary : colorScheme.onSurface,
+                        ),
                         overflow: TextOverflow.ellipsis,
                         maxLines: 1,
                       ),
                     ),
                     GestureDetector(
+                      behavior: HitTestBehavior.opaque,
+                      onTap: onClose,
                       child: Padding(
                         padding: const EdgeInsets.only(top: 2, right: 2),
                         child: Icon(
                           Icons.close,
                           size: 20,
-                          color: _isCurrent
+                          color: isCurrent
                               ? colorScheme.onPrimary.withOpacity(0.5)
                               : colorScheme.onSurface.withOpacity(0.5),
                         ),
                       ),
-                      behavior: HitTestBehavior.opaque,
-                      onTap: onClose,
                     ),
                   ],
                 ),
               ),
-              ...subtitle == null
-                  ? []
-                  : [
-                      Padding(
-                        padding: EdgeInsets.only(left: 8, right: 8, top: 0, bottom: 2),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Expanded(
-                              child: Text(
-                                subtitle,
-                                style: TextStyle(
-                                    fontSize: 12,
-                                    color: _isCurrent
-                                        ? colorScheme.onPrimary.withOpacity(0.5)
-                                        : colorScheme.onSurface.withOpacity(0.5)),
-                                overflow: TextOverflow.ellipsis,
-                                maxLines: 1,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-              Expanded(
-                child: Padding(
-                  padding: EdgeInsets.all(4),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
+              if (subtitle != null)
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(8, 0, 8, 2),
+                  child: Row(
                     children: [
                       Expanded(
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(8),
-                          child: _tab.previewImage == null
-                              ? Container(
-                                  color: colorScheme.surface,
-                                  child: Center(
-                                    child: Text(
-                                      'No preview',
-                                      style: TextStyle(
-                                        color: colorScheme.onSurface.withOpacity(0.5),
-                                        fontSize: 10,
-                                      ),
-                                    ),
-                                  ),
-                                )
-                              : UiImageWidget(image: _tab.previewImage!, fit: BoxFit.fitWidth),
+                        child: Text(
+                          subtitle,
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: isCurrent
+                                ? colorScheme.onPrimary.withOpacity(0.5)
+                                : colorScheme.onSurface.withOpacity(0.5),
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
                         ),
                       ),
                     ],
+                  ),
+                ),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(4),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: tab.previewImage == null
+                        ? ColoredBox(
+                      color: colorScheme.surface,
+                      child: Center(
+                        child: Text(
+                          'No preview',
+                          style: TextStyle(
+                            color: colorScheme.onSurface.withOpacity(0.5),
+                            fontSize: 10,
+                          ),
+                        ),
+                      ),
+                    )
+                        : UiImageWidget(image: tab.previewImage!, fit: BoxFit.fitWidth),
                   ),
                 ),
               ),
