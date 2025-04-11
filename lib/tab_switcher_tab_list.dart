99d5243aea8d2780449c2ac9bb3882fae9bcab3e
Mahdi Pishguy <pishguy@gmail.com>
2025-04-11 11:25:44 +0330

    Update tab_switcher_tab_list.dart

--- lib/tab_switcher_tab_list.dart
+++ lib/tab_switcher_tab_list.dart
@@ -7,11 +7,11 @@ import 'package:tab_switcher/tab_switcher_minimized_tab.dart';
 
 /// Displays list of minimized tabs
 class TabSwitcherTabList extends StatefulWidget {
-  TabSwitcherTabList(this.controller);
+  const TabSwitcherTabList(this.controller, {super.key});
 
   final TabSwitcherController controller;
 
-  static double kTabHeight = 148;
+  static const double kTabHeight = 148;
 
   @override
   State<TabSwitcherTabList> createState() => _TabSwitcherTabListState();
@@ -19,30 +19,33 @@ class TabSwitcherTabList extends StatefulWidget {
 
 class _TabSwitcherTabListState extends State<TabSwitcherTabList> {
   final GlobalKey<AnimatedListState> animatedListKey = GlobalKey<AnimatedListState>();
+  late StreamSubscription<TabSwitcherTab> _addSub;
+  late StreamSubscription<TabSwitcherTab> _closeSub;
 
   @override
   void initState() {
     super.initState();
     _addSub = widget.controller.onNewTab.listen((tab) {
-      animatedListKey.currentState?.insertItem(tab.index, duration: Duration(milliseconds: 1));
+      animatedListKey.currentState?.insertItem(tab.index, duration: const Duration(milliseconds: 1));
     });
     _closeSub = widget.controller.onTabClosed.listen((tab) {
       animatedListKey.currentState?.removeItem(
-          tab.index,
-          (context, animation) => SizeTransition(
-                axis: Axis.vertical,
-                sizeFactor: CurvedAnimation(parent: animation, curve: Curves.ease),
-                child: SizedBox(height: TabSwitcherTabList.kTabHeight),
-              ),
-          duration: Duration(milliseconds: 150));
+        tab.index,
+            (context, animation) => SizeTransition(
+          axis: Axis.vertical,
+          sizeFactor: CurvedAnimation(parent: animation, curve: Curves.ease),
+          child: const SizedBox(height: TabSwitcherTabList.kTabHeight),
+        ),
+        duration: const Duration(milliseconds: 150),
+      );
     });
   }
 
   @override
   void dispose() {
-    super.dispose();
     _addSub.cancel();
     _closeSub.cancel();
+    super.dispose();
   }
 
   @override
@@ -50,21 +53,21 @@ class _TabSwitcherTabListState extends State<TabSwitcherTabList> {
     return ScrollShadowContainer(
       child: Scrollbar(
         child: AnimatedList(
-          physics: BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+          physics: const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
           key: animatedListKey,
           itemBuilder: (context, i, animation) => SizeTransition(
             sizeFactor: animation,
             child: SlideTransition(
               position: animation.drive(Tween(
-                begin: Offset(-150, 0),
-                end: Offset(0, 0),
+                begin: const Offset(-1, 0),
+                end: Offset.zero,
               )),
-              child: Container(
+              child: SizedBox(
                 height: TabSwitcherTabList.kTabHeight,
                 child: TabSwitcherMinimizedTab(
                   widget.controller.tabs[i],
-                  () => widget.controller.switchToTab(i),
-                  () => widget.controller.closeTab(widget.controller.tabs[i]),
+                      () => widget.controller.switchToTab(i),
+                      () => widget.controller.closeTab(widget.controller.tabs[i]),
                   widget.controller.tabs[i] == widget.controller.currentTab,
                 ),
               ),
@@ -75,7 +78,4 @@ class _TabSwitcherTabListState extends State<TabSwitcherTabList> {
       ),
     );
   }
-
-  late StreamSubscription<TabSwitcherTab> _addSub;
-  late StreamSubscription<TabSwitcherTab> _closeSub;
 }
